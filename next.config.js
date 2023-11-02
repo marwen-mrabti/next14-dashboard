82571f7c249286038b9ce4fe8a1db5c6309db14e
marwen <marwen.mrabti.me@gmail.com>
2023-11-02 06:00:34 +0100

    images config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+	images: {
+		domains: ["https://next14-dashboard-marwen.vercel.app/"] // Add your domain here
+	}
 };
 
 module.exports = nextConfig;
